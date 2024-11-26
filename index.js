92d023c46ec70755b3b619da84c12706911e01ac
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-26 21:23:39 +0600

    Created an echo req with params and installed caesar salad.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/:echo', (req, res) => {
+    const echo = req.params.echo
+    res.send(echo);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port: http://localhost:${port}/`);
 });
