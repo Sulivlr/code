babd3773c5e3f2b908a1f28e68f751cfde5b96ca
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-26 22:14:19 +0600

    Created vigenere decode and encoded password with params.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Vigenere = require("caesar-salad/vigenere");
 
 const app = express();
 const port = 8000;
@@ -6,6 +7,17 @@ const port = 8000;
 app.get('/:echo', (req, res) => {
     const echo = req.params.echo
     res.send(echo);
+});
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encodedText = Vigenere.Cipher('password').crypt(text);
+    res.send(`<h1>Encoded Text: ${encodedText}</h1>`);
+});
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decodedText = Vigenere.Decipher('password').crypt(text);
+    res.send(`<h1>Decoded Text: ${decodedText}</h1>`);
 });
 
 app.listen(port, () => {
